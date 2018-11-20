ca5b573582e6b333a8342ab1a1651ee00b0ea2a3
Serrones <fabioserrones@gmail.com>
2018-11-20 10:04:46 -0200

    Django Testing

--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Board
 
 def home(request):
-    print('request: ', request)
-    print('request type: ', type(request))
-    print('request dir: ', dir(request))
-    print('request scheme: ', request.scheme)
-    print('request method: ', request.method)
-    print('request session: ', request.session)
-    return HttpResponse('Hey World!!')
+    boards = Board.objects.all()
+
+    return render(request, 'home.html', {'boards': boards})
